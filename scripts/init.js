572c406423d8c724138b7344cb3c6398eb8cd52f
whirling-in-rags <72390280+nschoenwald@users.noreply.github.com>
2026-02-09 19:05:31 +0100

    13.06

--- scripts/init.js
+++ scripts/init.js
@@ -2,7 +2,6 @@ import { registerSettings } from "./settings.js";
 import { ClipboardSystem } from "./systems/clipboard.js";
 import { ChatSystem } from "./systems/chat.js";
 import { ContextMenuSystem } from "./systems/context-menu.js";
-import { SceneToolsSystem } from "./systems/tools.js";
 
 Hooks.once("init", async () => {
     console.log("Nik's Show & Tell | Initializing...");
@@ -18,6 +17,3 @@ Hooks.once("ready", () => {
 Hooks.on("renderTileConfig", (app, html) => {
     ContextMenuSystem.renderTileConfig(app, html);
 });
-
-// Scene Tools hooks are better initialized early or via static init if they hook themselves
-SceneToolsSystem.init();
